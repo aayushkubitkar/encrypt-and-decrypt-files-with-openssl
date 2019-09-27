07139af3d2374fe7b0d9d7b3312c3c405230212d
aayush kubitkar <kubitkar.a@husky.neu.edu>
2019-09-27 15:16:01 -0400

    error handling

--- crypto.sh
+++ crypto.sh
@@ -19,36 +19,57 @@ decrypt()
 }
 usage()
 {
-	echo -e "Usage: \n Encryption:"
-	echo "  ./crypto.sh -e <receiver_public_key_file> <sender_private_key_file> <plaintext_file> <encrypted_file>"
-	echo " Decryption: "
-	echo "  ./crypto.sh -d <receiver_private_key_file> <sender_public_key_file> <encrypted_file> <decrypted_file>"
+	err=$1
+	if [ "$err" == "help" ]; then 
+		echo -e "Usage: \n Encryption:"
+		echo "  ./crypto.sh [-e] [receiver_public_key_file] [sender_private_key_file] [plaintext_file] [encrypted_file]"
+		echo " Decryption: "
+		echo "  ./crypto.sh [-d] [receiver_private_key_file[ [sender_public_key_file] [encrypted_file] [decrypted_file]"
+	elif  [ "$err" == "number of args" ]; then
+		args=$2
+		echo "Arguments found $args : Arguments required 4"
+	elif [ "$err" == "invalid file" ]; then
+		echo "File does not exist"
+	elif [ "$err" == "invalid option" ]; then
+		echo -e "Invalid option used! \n Usage : script [-e] | [-d]"
+	elif [ "$err" == "no option" ]; then
+		echo -e "No option specified! \n Usage : script [-e] | [-d]"
+	fi
 	exit 1;
 }
 
-if [[ $# -eq 5 ]]; then
-  while getopts "he:d:" options; do
+operation=""
+  while getopts ":hed" options; do
     case ${options} in
-	 	e) 
-	 	  if [ -f $2 -a -f $3 -a -f $4 -a -f $5 ]; then
-	 	   encrypt $2 $3 $4 $5
-          else
-          	echo "argument passed is not a file!"
-          fi
+	 	e ) 
+	 	  operation="e"
+	 	  ;;
+		d ) 
+	 	  operation="d"
 		  ;;
-		d) 
-	      if [ -f $2 -a -f $3 -a -f $4 -a -f $5 ]; then
-	       decrypt $2 $3 $4 $5
-	      else
-	       echo "argument passed is not a file!" 
-	      fi 
+		h ) 
+		  usage "help"
 		  ;;
-		h | *) 
-		  usage
+		? )
+		  usage "invalid option" 
 		  ;;
 	esac
   done
+if [ $((OPTIND-1)) -eq 0 ]; then
+	usage "no option"
+fi  
+shift "$((OPTIND -1))"
+if [ "$#" -eq 4 ]; then #checking the number of arguments 
+    if [ -f $1 -a -f $2 -a -f $3 -a -f $4 ]; then #checking if all the files passed as argument exists or not
+       if [ "$operation" == "e" ]; then
+       	encrypt $1 $2 $3 $4
+       elif [ "$operation" == "d" ]; then
+       	decrypt $1 $2 $3 $4
+       fi
+	else
+		usage "invalid file"
+	fi
 else
-	usage
+    usage "number of args" "$#"
 fi
 
