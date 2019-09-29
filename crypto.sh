208c2e2a92133c49f79c36cfbd93ed3e358c50b5
aayush kubitkar <kubitkar.a@husky.neu.edu>
2019-09-28 22:36:13 -0400

    adding comments

--- crypto.sh
+++ crypto.sh
@@ -6,26 +6,31 @@ encrypt()
 	 s_priv_key=$2
 	 p_file=$3
 	 e_file=$4
-	 echo "in encrypt function $r_pub_key, $s_priv_key, $p_file, $e_file"
+	 #echo "In encrypt function"
 	 
 	 workdir=`pwd`
 	 touch rand.session.key
-	 openssl rand -base64 32 > rand.session.key # generate 32 bit random session key
+	 # generate 32 bit random session key
+	 openssl rand -base64 32 > rand.session.key
 
 	 touch file.enc
+	 #symmetric encryption using AES 256
 	 openssl enc -aes-256-cbc -A -in $3 -out file.enc -pass file:./rand.session.key -salt
 
 	 touch session.key.enc
+	 #Asymmetric encryption session key with receiver's public key
 	 openssl rsautl -encrypt -pubin -inkey $1 -in rand.session.key -out session.key.enc
-
 	 rm rand.session.key
 
+	 #zip the session key and encryted file
 	 tar -czvf file.tar.gz session.key.enc file.enc
 	 rm session.key.enc file.enc
 
 	 touch file.dgst
+	 #Sign the tar file with senders public key using sha256 
 	 openssl dgst -sha256 -sign $2 -out file.dgst file.tar.gz
 
+	 #zip the digest and the original message
 	 tar -czvf $4 file.dgst file.tar.gz
 	 rm file.dgst file.tar.gz
 
@@ -36,21 +41,24 @@ decrypt()
 	 s_pub_key=$2
 	 e_file=$3
 	 p_file=$4
-	 echo "in decrypt function"
-	 
+	 #echo "in decrypt function"
 	 touch ver_status
 	 tar -xvf $3
+	 #verify the digest with the original message
 	 openssl dgst -sha256 -verify $2 -signature file.dgst file.tar.gz  > ver_status
-
+	 #check if the file was successfullly verified
 	 if grep -Fq "OK" ver_status ; then
-	  echo "verified"
-	  tar -xvf file.tar.gz
-	  touch decrypted.session.key
-      openssl rsautl -decrypt -inkey $1 -in session.key.enc -out decrypted.session.key
-	  openssl enc -aes-256-cbc -d -A -in file.enc -out $4 -pass file:./decrypted.session.key -salt 
-	  rm file.tar.gz file.dgst file.enc session.key.enc decrypted.session.key ver_status
+	  	#echo "verified"
+	  	tar -xvf file.tar.gz
+	  	touch decrypted.session.key
+	  	#decrypt the session key with the receivers private key
+      	openssl rsautl -decrypt -inkey $1 -in session.key.enc -out decrypted.session.key
+      	#decrypt the encrypted file with session key
+	  	openssl enc -aes-256-cbc -d -A -in file.enc -out $4 -pass file:./decrypted.session.key -salt
+	  	rm file.tar.gz file.dgst file.enc session.key.enc decrypted.session.key ver_status
 	 else
-	 	echo "signature not matching!"
+	 	echo "signature not verified"
+	 	echo "Original message and the digest do not match"
 	 fi
 }
 usage()
@@ -96,7 +104,7 @@ if [ $((OPTIND-1)) -eq 0 ]; then
 fi  
 shift "$((OPTIND -1))"
 if [ "$#" -eq 4 ]; then #checking the number of arguments 
-    if [ -f "$1" -a -f "$2" -a -f "$3" ]; then #checking if all the files passed as argument exists or not
+    if [ -f "$1" -a -f "$2" -a -f "$3" ]; then #checking if the files passed as argument exists or not
        if [ "$operation" == "e" ]; then
        	encrypt $1 $2 $3 $4
        elif [ "$operation" == "d" ]; then
